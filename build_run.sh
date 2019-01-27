b34c2973eb38018fd17fa5aac2b33a8ec194edb9
Neil Hazra <neilhazra001@gmail.com>
2019-01-27 12:32:14 -0800

    Rename Batch Files

--- build_run.sh
+++ build_run.sh
@@ -1,2 +1,2 @@
-nvcc jetson_image_processing.cu -o compiled.o `pkg-config --cflags --libs opencv` --std=c++11
+nvcc main_image_processing.cu -o compiled.o `pkg-config --cflags --libs opencv` --std=c++11
 ./compiled.o
--- camera.py
+++ camera.py
@@ -1,17 +1,10 @@
 import time
 import cv2
 
-# Cam properties
-fps = 1
-frame_width = 640
-frame_height = 480
+
 # Create capture
-cap = cv2.VideoCapture(1)
-# Set camera properties
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-cap.set(cv2.CAP_PROP_FPS, fps)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0);
+cap = cv2.VideoCapture("PUT GST STRING")
+
 # Check if cap is open
 if cap.isOpened() is not True:
     quit()
@@ -25,8 +18,6 @@ while True:
     ret, frame = cap.read()
     # Check
     if ret is True:
-        # Flip frame
-        frame = cv2.flip(frame, 1)
         cv2.imwrite("C:\\Users\\NeilHazra\\chessboard\\" + str(i) +".jpg", frame)
         cv2.imshow("video", frame);
         cv2.waitKey(100)
